8cb17c80da38b6c92dc5ad16e44e0d2705b7ce44
Mac <aqibafridi0345@gmail.com>
2022-09-24 17:21:27 +0500

    todo app design

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,4 +1,22 @@
 // event handler is a function which allow us to manage api. It uses H3 framework which is used to build rest apis.
 export default defineEventHandler( ()=>{
-    return "Hello its me aqib"
+    let todo = [
+        {
+            id: 1,
+            title: "Hello there !",
+            category: '',
+            completed: false,
+        },
+        {
+            id: 2,
+            name: "Hello there one !",
+            completed: false,
+        },
+        {
+            id: 3,
+            name: "Hello there two !",
+            completed: false,
+        }
+    ];
+    return todo;
 } )
