123bc4fba630e93a67e7c25bbd5f6aa7bca15eb2
Mac <aqibafridi0345@gmail.com>
2022-09-24 13:22:59 +0500

    now oue first api is created

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+  // {data} is used for destructing data fromm useFetch method
+  let {data} = useFetch('/api/todo');
+</script>
+
 <template>
   <div>
+    {{data}}
     <NuxtWelcome />
   </div>
 </template>
---
+++ server/api/todo/index.ts
@@ -0,0 +1,4 @@
+// event handler is a function which allow us to manage api. It uses H3 framework which is used to build rest apis.
+export default defineEventHandler( ()=>{
+    return "Hello its me aqib"
+} )
