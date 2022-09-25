e66dfbad30bd3193bb7d01beccdfd4f601a184f1
Mac <aqibafridi0345@gmail.com>
2022-09-25 11:38:37 +0500

    put request pending

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,15 +1,27 @@
 // event handler is a function which allow us to manage api. It uses H3 framework which is used to build rest apis.
+import { Body } from 'nuxt/dist/head/runtime/components';
 import { db } from '../../db/index'
-export default defineEventHandler( (e)=>{
+export default defineEventHandler( async (e)=>{
     let request = e.req.method;
     // get request 
     if(request === "GET")
         return db.todo;
-
-        console.log("request ended")
     // post request
     if(request === "POST"){
+        let data = await useBody(e);
+        if(!data) throw new Error("data has been empty")
+        console.log(data)
+        const newTodo = {
+            id: new Date().toISOString(),
+            title: data.title,
+            category: data.category,
+            completed: data.completed,
+        }
+        console.log(newTodo)
 
+        db.todo.push(newTodo);
+
+        return data;
     }
     // put request
 
--- server/db/index.ts
+++ server/db/index.ts
@@ -1,19 +1,19 @@
 export const db = {
     todo : [
         {
-            id: 1,
+            id: '1',
             title: "Hello there 1 !",
             category: true,
             completed: false,
         },
         {
-            id: 2,
+            id: '2',
             title: "Hello there 2 !",
             category: false,
             completed: false,
         },
         {
-            id: 3,
+            id: '3',
             title: "Hello there 3 !",
             category: true,
             completed: true,
